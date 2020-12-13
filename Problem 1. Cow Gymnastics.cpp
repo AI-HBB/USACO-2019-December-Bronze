fc13606c010f7cb29e007614a7885261c92af54d
AI-HBB <75945892+AI-HBB@users.noreply.github.com>
2020-12-13 13:23:38 -0500

    Update Problem 1. Cow Gymnastics.cpp

--- Problem 1. Cow Gymnastics.cpp
+++ Problem 1. Cow Gymnastics.cpp
@@ -9,30 +9,27 @@ cin >> K >> N;
 for(int i=0; i<K; i++){
   for(int k=0; k<N; k++){
    cin >> ar[i][k];
+  }
 }
-}
-  for(int k=0; k<N-1; k++){
-   for(int j=k+1; j<N; j++){
-     ans=1;
-     a=ar[0][k];
-     b=ar[0][j];
-     
-     for(int z=1; z<K; z++){   
-      for(int x=0; x<N; x++){
-       if(a==ar[z][x]){
-         a1=x;
-       }
-       if(b==ar[z][x]){
-         b1=x;
-       }
+for(int k=0; k<N-1; k++){
+ for(int j=k+1; j<N; j++){
+  ans=1;
+  a=ar[0][k];
+  b=ar[0][j];
+   for(int z=1; z<K; z++){   
+    for(int x=0; x<N; x++){
+      if(a==ar[z][x]){
+         a1=x;}
+      if(b==ar[z][x]){
+         b1=x;}
        }
        if(a1<b1){
         ans++;}
-  }
-  if(ans==K){
-       ansm++;}
-  }
-  } 
-cout << ansm;
-}
+         }
+        if(ans==K){
+         ansm++;}
+          }
+           } 
+           cout << ansm;
+            }
 
